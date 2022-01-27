d3e9703bf506d3d63511625edfee43c6006bc079
Mallikarjun362 <mallikarjun307@gmail.com>
2022-01-27 13:59:58 +0530

    added more nav bar elements

--- utils/requests.js
+++ utils/requests.js
@@ -29,22 +29,22 @@ export default {
         title : 'Mystery',
         url : `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
     },
-    // fetchSciFi : {
-    //     title : 'Sci Fi',
-    //     url : '/discover/movie?api_key=${API_KEY}&with_genres=28',
-    // },
-    // fetchWestern : {
-    //     title : 'Western',
-    //     url : '/discover/movie?api_key=${API_KEY}&with_genres=28',
-    // },
-    // fetchAnimation: {
-    //     title : 'Animation',
-    //     url : '/discover/movie?api_key=${API_KEY}&with_genres=28',
-    // },
-    // fetchTV: {
-    //     title : '',
-    //     url : '/discover/movie?api_key=${API_KEY}&with_genres=28',
-    // },
+    fetchSciFi : {
+        title : 'Sci Fi',
+        url : '/discover/movie?api_key=${API_KEY}&with_genres=878',
+    },
+    fetchWestern : {
+        title : 'Western',
+        url : '/discover/movie?api_key=${API_KEY}&with_genres=37',
+    },
+    fetchAnimation: {
+        title : 'Animation',
+        url : '/discover/movie?api_key=${API_KEY}&with_genres=16',
+    },
+    fetchTV: {
+        title : '',
+        url : '/discover/movie?api_key=${API_KEY}&with_genres=10770',
+    },
 };
 
 /*https://api.themoviedb.org/3
